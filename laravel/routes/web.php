964a1f57034c92765718cdd38516f5327297fa7a
adnane <qnaisadnane@gmail.com>
2026-02-05 21:47:08 +0100

    add app.css

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
